e178d253e53809aea5b4e86c9d83eb2b5c449d74
Jimmy Ostler <jtostler1@gmail.com>
2024-10-07 18:04:38 -0700

    simplified some more

--- src/eval.rs
+++ src/eval.rs
@@ -1,4 +1,4 @@
-use crate::{lexer::Operator, parser::{Expr, Value}};
+use crate::{lexer::Operator, parser::Expr};
 
 /// We want to figure out what our expression actually is. To do this, we recursively traverse our
 /// AST. When we get to a number, we return it, and we eventually get to a case where we meet
@@ -7,16 +7,18 @@ use crate::{lexer::Operator, parser::{Expr, Value}};
 pub fn eval_expr(expr: Expr) -> f64 {
     match expr {
         Expr::Number(num) => num,
-        Expr::Value(value) => eval_value(*value),
-    }
-}
-
-fn eval_value(val: Value) -> f64 {
-    match val.operation {
-        Operator::Add => eval_expr(*val.left_operand) + eval_expr(*val.right_operand),
-        Operator::Mul => eval_expr(*val.left_operand) * eval_expr(*val.right_operand),
-        Operator::Sub => eval_expr(*val.left_operand) - eval_expr(*val.right_operand),
-        Operator::Div => eval_expr(*val.left_operand) / eval_expr(*val.right_operand),
-        Operator::Pow => eval_expr(*val.left_operand).powf(eval_expr(*val.right_operand)),
+        Expr::Value {
+            operation,
+            left_operand,
+            right_operand
+        } => {
+            match operation {
+                Operator::Add => eval_expr(*left_operand) + eval_expr(*right_operand),
+                Operator::Mul => eval_expr(*left_operand) * eval_expr(*right_operand),
+                Operator::Sub => eval_expr(*left_operand) - eval_expr(*right_operand),
+                Operator::Div => eval_expr(*left_operand) / eval_expr(*right_operand),
+                Operator::Pow => eval_expr(*left_operand).powf(eval_expr(*right_operand)),
+            }
+        }
     }
 }
--- src/parser.rs
+++ src/parser.rs
@@ -2,30 +2,29 @@ use std::collections::VecDeque;
 
 use crate::lexer::{Operator, Token};
 
+// NOTE: This section is a lot. Read it slowly, and take it piece by piece.
+
 /// Our expression can be defined recursively. 
-/// Each expression is either a branching expression (+ 9 7) or a number (9). 
+/// Each expression is either a binary operation (+ 9 7), or a number (9). 
 /// For example: (+ 9 0) => 
 /// ```
-/// Expr::(Value {
+/// Expr::Value {
 ///     operation: Add, 
 ///     left: Expr::Number(9), 
-///     right Expr::Number(0) 
-/// })
+///     right: Expr::Number(0),
+/// }
 /// ```
-/// We see our Expr contains an Expr.
+/// We see our Expr with the operation contains an Expr representing the numbers.
 #[derive(Debug)]
 pub enum Expr {
     // Recursive, as value contains 2 expressions, with Number case being the terminal base case
     // The Box<> can be ignored, it simply provides some help for the compiler.
-    Value(Box<Value>),
+    Value {
+        operation: Operator,
+        left_operand: Box<Expr>,
+        right_operand: Box<Expr>,
+    },
     Number(f64),
-}
-
-#[derive(Debug)]
-pub struct Value {
-    pub operation: Operator,
-    pub left_operand: Box<Expr>,
-    pub right_operand: Box<Expr>,
 }
 
 /// Parses a token string into a valid expression
@@ -35,11 +34,11 @@ pub fn parse(token_string: &mut VecDeque<Token>) -> Expr {
         Token::Operator(operation) => {
             let left_operand = Box::new(parse(token_string));
             let right_operand = Box::new(parse(token_string));
-            Expr::Value(Box::new(Value {
+            Expr::Value {
                 operation,
                 left_operand,
                 right_operand
-            }))
+            }
         }
     }
 }
